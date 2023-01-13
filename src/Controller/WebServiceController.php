ab74931ad48cd84007328036187189d86d1d99c5
EGOUI <egouinguenet@logtimiz.com>
2023-01-13 05:49:24 +0100

    Initial exercise creation

---
+++ src/Controller/WebServiceController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebServiceController extends AbstractController
+{
+    #[Route('/web/service/ZWSLOC', name: 'app_web_service_ZWSLOC')]
+    public function ZWSLOC(): Response
+    {
+        // Extra step: override default I_STOFCY with query param if it does exist.
+
+        $webServiceName         = 'ZWSLOC';
+        $webServiceRequestParam = [
+            'PARAM_IN' => [
+                'I_STOFCY' => 'FR011',
+                'I_LOC'    => ''
+            ]
+        ];
+
+        // TODO : Execute web service soap call
+
+        // TODO : Extract data from the response
+
+        return $this->render('transaction/' . $webServiceName . '/index.html.twig', [
+            'controller_name' => 'WebServiceController',
+            // TODO : Pass data to Twig
+        ]);
+    }
+
+    #[Route('/web/service/ZWSSTOCK', name: 'app_web_service_ZWSSTOCK')]
+    public function ZWSSTOCK(): Response
+    {
+        $queryLocalisation = ''; // TODO: Extract localisation from query parameter
+
+        $webServiceName         = 'ZWSSTOCK';
+        $webServiceRequestParam = [
+            "PARAM_IN" => [
+                "I_STOFCY" => "FR011",
+                "I_ITMREF" => "",
+                "I_LOT"    => "",
+                "I_LOC"    => $queryLocalisation
+            ]
+        ];
+
+        // Execute web service soap call
+
+        // Extract data from the response
+
+        return $this->render('transaction/' . $webServiceName . '/index.html.twig', [
+            'controller_name' => 'WebServiceController',
+            // Pass data to Twig
+        ]);
+    }
+}
